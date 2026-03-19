7f97743c14a1e165b8b0e4b86ff19df0ccefbad3
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2026-03-18 16:53:37 -1000

    run em model without platoons

--- extract_opakapaka_ss3_truth.r
+++ extract_opakapaka_ss3_truth.r
@@ -11,6 +11,7 @@ library(magrittr)
 library(tibble)
 
 dir_ss3_em <- file.path(getwd(), "HRF_SQ_25_yrfwd","1","em")
+dir_ss3_em_no_platoon <- file.path(getwd(), "HRF_SQ_25_yrfwd","1","em_no_platoon")
 dir_ss3_om <- file.path(getwd(), "HRF_SQ_25_yrfwd","1","om")
 dir_out <- file.path(getwd(), "opal_data", "opakapaka_ss3")
 
@@ -18,14 +19,18 @@ last_hist_yr <- 2023
 
 om_results = r4ss::SS_output(dir_ss3_om, printstats = FALSE, verbose = FALSE)
 em_results = r4ss::SS_output(dir_ss3_em, printstats = FALSE, verbose = FALSE)
+em_no_platoon_results = r4ss::SS_output(dir_ss3_em_no_platoon, printstats = FALSE, verbose = FALSE)
 
-opaka_truth = list(om = list(),em=list())
+opaka_truth = list(om = list(),em=list(), em_no_platoon = list())
 opaka_truth$om$sprseries = om_results$sprseries[,c("Yr","SSB")]
 opaka_truth$em$sprseries = em_results$sprseries[,c("Yr","SSB")]
+opaka_truth$em_no_platoon$sprseries = em_no_platoon_results$sprseries[,c("Yr","SSB")]
 opaka_truth$om$cpue = om_results$cpue[,c("Yr","Fleet","Fleet_name","Vuln_bio","Obs","Exp","SE")]
 opaka_truth$em$cpue = em_results$cpue[,c("Yr","Fleet","Fleet_name","Vuln_bio","Obs","Exp","SE")]
+opaka_truth$em_no_platoon$cpue = em_no_platoon_results$cpue[,c("Yr","Fleet","Fleet_name","Vuln_bio","Obs","Exp","SE")]
 
 save(opaka_truth, file = file.path(dir_out, "opaka_truth.rda"))
 
 plot(opaka_truth$om$sprseries$Yr, opaka_truth$om$sprseries$SSB, type = "l")
 lines(opaka_truth$em$sprseries$Yr, opaka_truth$em$sprseries$SSB, col = "red")
+lines(opaka_truth$em_no_platoon$sprseries$Yr, opaka_truth$em_no_platoon$sprseries$SSB, col = "blue")
